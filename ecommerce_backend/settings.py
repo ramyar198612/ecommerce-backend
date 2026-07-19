b14e7786d511e8676e2cf3606fe3f73772c4cbdb
ramyar198612 <ramyar198612@gmail.com>
2026-07-19 16:36:39 +0530

    Switch back to SQLite

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -79,15 +79,12 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-import os
-import dj_database_url
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
